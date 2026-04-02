6601d2ce18ae9ad6637d2b611566e7fa8d70f4f4
FrankOHara43 <aryaswaraj5@gmail.com>
2026-04-02 06:28:56 +0530

    account microservice(partially completed) with main.go
    Signed-off-by: FrankOHara43 <aryaswaraj5@gmail.com>

--- account/cmd/account/main.go
+++ account/cmd/account/main.go
@@ -1,1 +1,36 @@
-package account
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/FrankOHara43/go-grpc-microservice/account"
+	"github.com/kelseyhightower/envconfig"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `env:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("",&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r account.Respository
+	retry.ForeverSleep(2*time.Second, func(_ int)(err error){
+		r, err =account.NewPostgresRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return 
+	})
+	defer r.close()
+	log.Println("Listening on port 8080...")
+	s := account.NewService(r)
+	log.Fatal(account.ListenGRPC(s, 8080))
+}
